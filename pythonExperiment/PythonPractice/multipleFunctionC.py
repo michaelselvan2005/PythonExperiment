d1e25b9aa57839fd2eada10a484e21e795471005
Michael Selvan <michael.selvan2005@gmail.com>
2024-04-09 04:45:43 +0530

    class

---
+++ pythonExperiment/PythonPractice/multipleFunctionC.py
@@ -0,0 +1,10 @@
+class Calculator():
+    def addition():
+        message=10+20
+        return message
+    def subraction():
+        message=10-20
+        return message
+    def multi():
+        message=10*20
+        return message
